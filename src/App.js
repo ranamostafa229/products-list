f52e27b8a3263c4bb7bd502c3472de20119a0159
Your Name <you@example.com>
2024-01-21 22:04:08 +0200

    add quantity column

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
-import './App.css';
-import React, {Component} from 'react';
-import Items from './components/item/items'
-import AddItem from './components/addItem/addItem'
-import Total from './components/total/total'
+import "./App.css";
+import React, { Component } from "react";
+import Items from "./components/item/items";
+import AddItem from "./components/addItem/addItem";
+import Total from "./components/total/total";
 
 class App extends Component {
   state = {
     items: [
-      {id:1, product:'Pen', price:2},
-      {id:2, product:'Book', price:10}
-    ]
-  }
+      { id: 1, product: "Pen", price: 2, qunatity: 1 },
+      { id: 2, product: "Book", price: 10, qunatity: 1 },
+    ],
+  };
 
   deleteItem = (id) => {
-    let items = this.state.items
-    let i = items.findIndex(item => item.id === id)
-    items.splice(i, 1)
-    this.setState({items: items})
-  }
+    let items = this.state.items;
+    let i = items.findIndex((item) => item.id === id);
+    items.splice(i, 1);
+    this.setState({ items: items });
+  };
 
   addItem = (item) => {
-    this.state.items.length > 0 ? (
-      item.id = this.state.items[this.state.items.length - 1].id + 1 
-    ) : item.id = 1
-    console.log(item.id)
-    let items = this.state.items
-    items.push(item)
-    this.setState({items: items})
-  }
+    this.state.items.length > 0
+      ? (item.id = this.state.items[this.state.items.length - 1].id + 1)
+      : (item.id = 1);
+    console.log(item.id);
+    let items = this.state.items;
+    items.push(item);
+    this.setState({ items: items });
+  };
+  addQunatity = (id) => {
+    let items = this.state.items;
+    let i = items.findIndex((item) => item.id === id);
+    let qunatity = items[i].qunatity++;
+    this.setState({ items: items, qunatity: qunatity });
+  };
+  minusQunatity = (id) => {
+    let items = this.state.items;
+    let i = items.findIndex((item) => item.id === id);
+    let qunatity = items[i].qunatity;
+    qunatity > 1 ? items[i].qunatity-- : this.deleteItem(id);
+    this.setState({ items: items, qunatity: qunatity });
+  };
 
   render() {
     return (
       <div className="container">
         <h1>Product List React App</h1>
         <div className="table">
-          <Items items={this.state.items} del={this.deleteItem}/>
-          <AddItem add={this.addItem}/>
-          <Total items={this.state.items}/>
+          <Items
+            items={this.state.items}
+            del={this.deleteItem}
+            add={this.addQunatity}
+            remove={this.minusQunatity}
+          />
+          <AddItem add={this.addItem} />
+          <Total items={this.state.items} />
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
-        </div>
-    )
-}
+  const { items, del, add, remove } = props;
+  let length = items.length;
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
+          <p>
+            <button className="minusButton" onClick={() => remove(item.id)}>
+              -
+            </button>
+            {item.qunatity}
+            <button className="addButton" onClick={() => add(item.id)}>
+              +
+            </button>
+          </p>
 
-export default Items
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
+        </div>
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Edit</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
+
+export default Items;
